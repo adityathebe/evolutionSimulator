959a23f9a64784b0d2feecef24cd5292d523028b
adityathebe <thebeaditya@gmail.com>
2018-05-15 17:34:28 +0545

    fixed collision filter

--- person.js
+++ person.js
@@ -1,7 +1,5 @@
 class Person {
 	constructor(upper_length, upper_width, lower_length, lower_width, x, y) {
-		this.x = x;
-		this.y = y;
 		this.upper_length = upper_length;
 		this.upper_width = upper_width;
 		this.lower_length = lower_length;
@@ -10,11 +8,13 @@ class Person {
 			friction: 0.8,
 			restitution: 0.2,
 			collisionFilter: {
+				category: 0x0002,
 				mask: 0x0001,
 			}
 		});
 		this.lower_left_leg = Matter.Bodies.rectangle(x, y + upper_length, lower_width, lower_length, {
 			collisionFilter: {
+				category: 0x0003,
 				mask: 0x0001,
 			},
 			friction: 0.8,
@@ -24,14 +24,16 @@ class Person {
 			friction: 0.8,
 			restitution: 0.2,
 			collisionFilter: {
-				mask: 0x0002,
+				category: 0x0004,
+				mask: 0x0001,
 			}
 		});
 		this.lower_right_leg = Matter.Bodies.rectangle(x, y + upper_length, lower_width, lower_length, {
 			friction: 0.8,
 			restitution: 0.2,
 			collisionFilter: {
-				category: 0x0002
+				category: 0x00010,
+				mask: 0x0001,
 			}
 		});
 	}
@@ -41,18 +43,18 @@ class Person {
 		this.left_joint = Matter.Constraint.create({
 			bodyA: this.upper_left_leg,
 			bodyB: this.lower_left_leg,
-			pointA: { x: 0, y: 25 },
-			pointB: { x: 0, y: -25 },
+			pointA: { x: 0, y: this.upper_length / 2 },
+			pointB: { x: 0, y: -this.upper_length / 2 },
 			stiffness: 1,
-			length: 1,			
+			length: 1,
 		});
 
 
 		this.right_joint = Matter.Constraint.create({
 			bodyA: this.upper_right_leg,
 			bodyB: this.lower_right_leg,
-			pointA: { x: 0, y: 25 },
-			pointB: { x: 0, y: -25 },
+			pointA: { x: 0, y: this.upper_length / 2 },
+			pointB: { x: 0, y: -this.upper_length / 2 },
 			length: 1,
 			stiffness: 1,
 		});
@@ -60,8 +62,8 @@ class Person {
 		this.main_joint = Matter.Constraint.create({
 			bodyA: this.upper_left_leg,
 			bodyB: this.upper_right_leg,
-			pointA: { x: 0, y: -25 },
-			pointB: { x: 0, y: -25 },
+			pointA: { x: 0, y: -this.upper_length / 2 },
+			pointB: { x: 0, y: -this.upper_length / 2 },
 			length: 1,
 		})
 	}
--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const engine = Matter.Engine.create();
 const world = engine.world;
 
 function setup() {
-	let canvas = createCanvas(800, 550);
+	let canvas = createCanvas(windowWidth * 0.8, windowHeight * 0.8);
 	frameRate(60);
 	rectMode(CENTER);	
 	
@@ -15,14 +15,21 @@ function setup() {
 	person.init();
 
 	// add all of the bodies to the world
-	ground = Matter.Bodies.rectangle(width / 2, height, width, 25, { isStatic: true, friction: 1 });
+	ground = Matter.Bodies.rectangle(width / 2, height, width, 40, { 
+		isStatic: true, 
+		friction: 0.8,
+		collisionFilter: {
+			category: 0x0001
+		}
+	});
 	Matter.World.add(world, [ground, person.lower_left_leg, person.upper_left_leg, person.lower_right_leg, person.upper_right_leg]);
 	Matter.World.add(world, person.left_joint)
 	Matter.World.add(world, person.right_joint)
 	Matter.World.add(world, person.main_joint)
 
 	// Mouse Constraint
-	let canvasMouse = Matter.Mouse.create(canvas.elt)
+	let canvasMouse = Matter.Mouse.create(canvas.elt);
+	canvasMouse.pixelRatio = pixelDensity();
 	let m = Matter.MouseConstraint.create(engine, {
 		mouse: canvasMouse
 	})
@@ -40,7 +47,7 @@ function draw() {
 
 	// Display Ground
 	fill(color(118, 240, 155))
-	rect(width / 2, height, width, 25);
+	rect(ground.position.x, ground.position.y, width, 40);
 
 	// Display Person
 	fill("#F55F5F")
