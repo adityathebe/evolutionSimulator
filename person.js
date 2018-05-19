c443852f994227229382fe846e1c4faa3b52001a
adityathebe <thebeaditya@gmail.com>
2018-05-19 13:15:48 +0545

    Added Neural Network & Muscles

--- person.js
+++ person.js
@@ -7,6 +7,7 @@ class Person {
 		this.upper_left_leg = Matter.Bodies.rectangle(x, y, upper_width, upper_length, {
 			friction: 0.8,
 			restitution: 0.2,
+			density: 0.001,
 			collisionFilter: {
 				category: 0x0002,
 				mask: 0x0001,
@@ -14,15 +15,17 @@ class Person {
 		});
 		this.lower_left_leg = Matter.Bodies.rectangle(x, y + upper_length, lower_width, lower_length, {
 			collisionFilter: {
-				category: 0x0003,
+				category: 0x0002,
 				mask: 0x0001,
 			},
 			friction: 0.8,
-			restitution: 0.2
+			density: 0.001,
+			restitution: 0.2,
 		});
 		this.upper_right_leg = Matter.Bodies.rectangle(x, y, upper_width, upper_length, {
 			friction: 0.8,
 			restitution: 0.2,
+			density: 0.001,
 			collisionFilter: {
 				category: 0x0004,
 				mask: 0x0001,
@@ -31,11 +34,15 @@ class Person {
 		this.lower_right_leg = Matter.Bodies.rectangle(x, y + upper_length, lower_width, lower_length, {
 			friction: 0.8,
 			restitution: 0.2,
+			density: 0.001,
 			collisionFilter: {
-				category: 0x00010,
+				category: 0x0004,
 				mask: 0x0001,
 			}
 		});
+
+		// Neural Network
+		this.brain = new NeuralNetwork(5, 10, 3);
 	}
 
 	init() {
@@ -45,8 +52,8 @@ class Person {
 			bodyB: this.lower_left_leg,
 			pointA: { x: 0, y: this.upper_length / 2 },
 			pointB: { x: 0, y: -this.upper_length / 2 },
+			length: 0,
 			stiffness: 1,
-			length: 1,
 		});
 
 
@@ -55,7 +62,7 @@ class Person {
 			bodyB: this.lower_right_leg,
 			pointA: { x: 0, y: this.upper_length / 2 },
 			pointB: { x: 0, y: -this.upper_length / 2 },
-			length: 1,
+			length: 0,
 			stiffness: 1,
 		});
 
@@ -64,28 +71,55 @@ class Person {
 			bodyB: this.upper_right_leg,
 			pointA: { x: 0, y: -this.upper_length / 2 },
 			pointB: { x: 0, y: -this.upper_length / 2 },
-			length: 1,
-		})
+			length: 0,
+			stiffness: 1,
+		});
+
+		// Muscle Joints
+		this.main_muscle = Matter.Constraint.create({
+			bodyA: this.upper_left_leg,
+			bodyB: this.upper_right_leg,
+			length: (this.upper_length / 2),
+			stiffness: 1,
+		});
+
+		this.left_muscle = Matter.Constraint.create({
+			bodyA: this.upper_left_leg,
+			bodyB: this.lower_left_leg,
+			length: (this.upper_length / 3) + (this.lower_length / 3),
+			stiffness: 1
+		});
+
+		this.right_muscle = Matter.Constraint.create({
+			bodyA: this.upper_right_leg,
+			bodyB: this.lower_right_leg,
+			length: (this.upper_length / 3) + (this.lower_length / 3),
+			stiffness: 1
+		});
 	}
 
 	show() {
+		fill('green')
 		beginShape();
 		for (let i = 0; i < 4; i++) {
 			vertex(this.upper_left_leg.vertices[i].x, this.upper_left_leg.vertices[i].y);
 		}
 		endShape();
 
+		fill('red')
 		beginShape();
 		for (let i = 0; i < 4; i++) {
 			vertex(this.upper_right_leg.vertices[i].x, this.upper_right_leg.vertices[i].y);
 		}
 		endShape();
 
+		fill('green');
 		beginShape();
 		for (let i = 0; i < 4; i++) {
 			vertex(this.lower_left_leg.vertices[i].x, this.lower_left_leg.vertices[i].y);
 		}
 		endShape();
+		fill('red')
 
 		beginShape();
 		for (let i = 0; i < 4; i++) {
@@ -93,4 +127,57 @@ class Person {
 		}
 		endShape();
 	}
+
+	// Movements
+	move_m1(change) {
+		let max = (this.upper_length / 2) + (this.lower_length / 2);
+		let temp = this.main_muscle.length + (change * max);
+		if (temp >= max) return false;
+		if (temp <= 0.01) return false;
+		this.main_muscle.length = temp
+	}
+
+	move_m2(change) {
+		let max = (this.upper_length / 2) + (this.lower_length / 2);
+		let temp = this.left_muscle.length + ( change * max );
+		if (temp <= 0.01) return false;
+		if (temp >= max) return false;
+		this.left_muscle.length = temp;
+	}
+
+	move_m3(change) {
+		let max = (this.upper_length / 2) + (this.lower_length / 2)
+		let temp = this.right_muscle.length + (change * max);
+		if (temp <= 0.01) return false;
+		if (temp >= max) return false;
+		this.right_muscle.length = temp;
+	}
+
+	think() {
+		let distance_from_ground = ground.position.y - ((creatures[0].upper_left_leg.position.y + creatures[0].upper_right_leg.position.y + creatures[0].lower_right_leg.position.y + creatures[0].lower_left_leg.position.y) / 4)
+		let torque = this.upper_left_leg.angularVelocity + this.upper_right_leg.angularVelocity + this.lower_right_leg.angularVelocity + this.lower_left_leg.angularVelocity;
+		let angle = this.upper_left_leg.angle + this.upper_right_leg.angle + this.lower_right_leg.angle + this.lower_left_leg.angle;
+		let vx = this.upper_left_leg.velocity.x + this.upper_right_leg.velocity.x + this.lower_right_leg.velocity.x + this.lower_left_leg.velocity.x;
+		let vy = this.upper_left_leg.velocity.y + this.upper_right_leg.velocity.y + this.lower_right_leg.velocity.y + this.lower_left_leg.velocity.y;
+		let input = [distance_from_ground / width, vx / 4, vy / 4, torque / 4, angle / (4 * PI)];
+
+		// let ground_touch = 
+		let result = this.brain.predict(input).dataSync();
+
+		this.move_m1(result[0])
+		this.move_m2(result[1])
+		this.move_m3(result[2])
+
+		// console.log(input)
+		console.log(result)
+	}
+
+	walk() {
+		setInterval(() => {
+			this.think()
+		}, 500)
+	}
 }
+
+
+ground.position.y - ((creatures[0].upper_left_leg.position.y + creatures[0].upper_right_leg.position.y + creatures[0].lower_right_leg.position.y + creatures[0].lower_left_leg.position.y) / 4)
--- sketch.js
+++ sketch.js
@@ -1,31 +1,73 @@
 let person;
-let ground;
+let ground, roof;
+let left_wall, right_wall;
+let creatures = []
 
 const Render = Matter.Render
 const engine = Matter.Engine.create();
 const world = engine.world;
 
 function setup() {
-	let canvas = createCanvas(windowWidth * 0.8, windowHeight * 0.8);
+	let canvas = createCanvas(windowWidth * 0.95, windowHeight * 0.95);
 	frameRate(60);
-	rectMode(CENTER);	
-	
-	// Create Person
-	person = new Person(50, 20, 50, 10, width / 2, 0);
-	person.init();
+	rectMode(CENTER);
 
-	// add all of the bodies to the world
-	ground = Matter.Bodies.rectangle(width / 2, height, width, 40, { 
-		isStatic: true, 
+	// Create Person
+	for (let index = 0; index < 20; index++) {
+		let person = new Person(30, 5, 30, 5, width * 0.10, height * 0.75);
+		person.init();
+		creatures.push(person);
+	}
+
+	// Boundaries
+	ground = Matter.Bodies.rectangle(width / 2, height - 10, width, 20, {
+		isStatic: true,
 		friction: 0.8,
 		collisionFilter: {
 			category: 0x0001
 		}
 	});
-	Matter.World.add(world, [ground, person.lower_left_leg, person.upper_left_leg, person.lower_right_leg, person.upper_right_leg]);
-	Matter.World.add(world, person.left_joint)
-	Matter.World.add(world, person.right_joint)
-	Matter.World.add(world, person.main_joint)
+
+	roof = Matter.Bodies.rectangle(width / 2, 10, width, 20, {
+		isStatic: true,
+		friction: 0.8,
+		collisionFilter: {
+			category: 0x0001
+		}
+	});
+
+	// Left Wall
+	left_wall = Matter.Bodies.rectangle(10, height/2, 20, height, {
+		isStatic: true,
+		friction: 0.8,
+		collisionFilter: {
+			category: 0x0001
+		}
+	});
+
+	// Left Wall
+	right_wall = Matter.Bodies.rectangle(width - 10, height / 2, 20, height, {
+		isStatic: true,
+		friction: 0.8,
+		collisionFilter: {
+			category: 0x0001
+		}
+	});
+
+	Matter.World.add(world, [ground, roof, left_wall, right_wall]);
+	creatures.forEach((person) => {
+		Matter.World.add(world, [person.lower_left_leg, person.upper_left_leg, person.lower_right_leg, person.upper_right_leg]);
+		
+		// Add Main Joints
+		Matter.World.add(world, person.left_joint)
+		Matter.World.add(world, person.right_joint)
+		Matter.World.add(world, person.main_joint)
+
+		// Add Muscle Joints
+		Matter.World.add(world, person.main_muscle)
+		Matter.World.add(world, person.left_muscle)
+		Matter.World.add(world, person.right_muscle)
+	})
 
 	// Mouse Constraint
 	let canvasMouse = Matter.Mouse.create(canvas.elt);
@@ -33,13 +75,24 @@ function setup() {
 	let m = Matter.MouseConstraint.create(engine, {
 		mouse: canvasMouse
 	})
-	Matter.World.add(world, m)
+	Matter.World.add(world, m);
 
+	
 	// Run the renderer
-	// Render.run(Render.create({
+	// let render = Render.create({
 	// 	element: document.body,
-	// 	engine: engine
-	// });
+	// 	engine: engine,
+	// 	options: {
+	// 		height, width
+	// 	}
+	// })
+	// Render.run(render);
+
+	// let renderMouse = Matter.Mouse.create(render.canvas);
+	// renderMouse.pixelRatio = pixelDensity();
+	// Matter.World.add(world, Matter.MouseConstraint.create(engine, {
+	// 	mouse: renderMouse
+	// }));
 }
 
 function draw() {
@@ -47,12 +100,33 @@ function draw() {
 
 	// Display Ground
 	fill(color(118, 240, 155))
-	rect(ground.position.x, ground.position.y, width, 40);
+	rect(ground.position.x, ground.position.y, width, 20);
+	rect(left_wall.position.x, left_wall.position.y, 20, height);
+	rect(right_wall.position.x, right_wall.position.y, 20, height);
+	rect(roof.position.x, roof.position.y, width, 20);
 
 	// Display Person
 	fill("#F55F5F")
-	person.show();
+	creatures.forEach((person) => {
+		person.show();
+	})
 
 	// Run Matter-JS Engine
 	Matter.Engine.update(engine);
+}
+
+function keyPressed() {
+	// Press SpaceBar to exert small force to all creatures
+	if (key === " ") {
+		creatures.forEach((person) => {
+			Matter.Body.applyForce(person.upper_left_leg, { x: 0, y: 0 }, { x: 0.001, y: 0 })
+		})
+	}
+
+	// Press W to activate Neural Network
+	if (key === "W") {
+		creatures.forEach((person) => {
+			person.walk();
+		})
+	}
 }
