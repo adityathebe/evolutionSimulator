c07cfaf5ace1adf7b2979a2188f15320dd557535
Aditya Thebe <contact@adityathebe.com>
2019-01-18 00:10:43 +0545

    Added Steps column in table && modified chart

--- src/ui.js
+++ src/ui.js
@@ -7,15 +7,17 @@ class UIHandler {
     const head = table.insertRow(-1);
     head.insertCell(-1).innerHTML = "Human";
     head.insertCell(-1).innerHTML = "Score";
+    head.insertCell(-1).innerHTML = "Steps";
     head.style.fontWeight = "bold";
 
     for (const human of globals.humans) {
       const row = table.insertRow(-1);
       row.insertCell(-1).innerHTML = ++index;
       row.insertCell(-1).innerHTML = human.score.toFixed(2);
+      row.insertCell(-1).innerHTML = human.stepsMade;
 
       if (human.isAlive === false) {
-        row.style.backgroundColor = "#e74c3c";
+        row.style.backgroundColor = "#FF2748";
       }
     }
   }
@@ -33,20 +35,37 @@ class UIHandler {
   }
 
   static displayChart() {
-    const labels = Array(globals.generationIndex).fill(null).map((x, i) => i)
-    const data = globals.generationHighScores.map(x => x.score);
+    const labels = Array(globals.generationIndex).fill(null).map((x, i) => i + 1)
+    const highScoreData = globals.generationHighScores.map(x => x.score);
+    const avgScoreData = globals.generationAvgScores;
     var ctx = document.getElementById("chart").getContext('2d');
     var myChart = new Chart(ctx, {
       type: 'line',
       data: {
         labels,
         datasets: [{
-          label: 'Progress Chart',
-          data,
+          label: 'High Score',
+          data: highScoreData,
           fill: false,
+          borderColor: 'red',
+          backgroundColor: 'red',
+          borderDash: [3, 1],
+          pointRadius: 1,
+          pointHoverRadius: 3,
+        },
+        {
+          label: 'Average Score',
+          data: avgScoreData,
+          fill: true,
+          borderColor: '#2193EE',
+          backgroundColor: '#2193EE',
+          pointRadius: 0,
+          pointHoverRadius: 0,
         }],
       },
       options: {
+        events: [], // Bug on chartJS /issues/3753
+        animation: false,
         responsive: true,
         tooltips: {
           mode: 'index',
@@ -64,7 +83,7 @@ class UIHandler {
             display: true,
             scaleLabel: {
               display: true,
-              labelString: 'Highest Score'
+              labelString: 'Score'
             }
           }]
         }
